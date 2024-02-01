5ff709e2609c32b0a0d15529854c127cad84ef43
silverskg <chokichoki_gm@i.softbank.jp>
2024-02-01 18:06:37 +0900

    header-style

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,35 @@
-jQuery(function ($) { // $はjQueryを表す
-    // この中にコードを書く
+jQuery(function ($) {
+    // ハンバーガーメニュー
+    $(function () {
+        $(".js-hamburger").on("click", function () {
+            $(this).toggleClass("is-open");
+            if ($(this).hasClass("is-open")) {
+                openDrawer();
+            } else {
+                closeDrawer();
+            }
+        });
 
-    // 例：クラスがc-btnのcolorを黒色にする
-    // $(".c-btn").css("color","black")
+        // backgroundまたはページ内リンクをクリックで閉じる
+        $(".js-drawer a[href]").on("click", function () {
+            closeDrawer();
+        });
 
-    // 例：idがbuttonのテキストを変更
-    // $("#button").text("ボタンのテキストを変更");
+        // resizeイベント
+        $(window).on('resize', function() {
+            if (window.matchMedia("(min-width: 768px)").matches) {
+                closeDrawer();
+            }
+        });
+    });
 
-    // 例：クラスがc-btnをクリックした場合、コンソールログを出力
-    // $(".c-btn").click(function() {
-    //     console.log("ボタンがクリックされました！");
-    // });
+    function openDrawer() {
+        $(".js-drawer").addClass("is-open");
+        $(".js-hamburger").addClass("is-open");
+    }
+
+    function closeDrawer() {
+        $(".js-drawer").removeClass("is-open");
+        $(".js-hamburger").removeClass("is-open");
+    }
 });
