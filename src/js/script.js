b0c16663e91d21877d106f18109f5b25dd3e4631
silverskg <chokichoki_gm@i.softbank.jp>
2024-02-05 21:18:56 +0900

    ワイヤーフレーム-コーディング

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,10 @@ jQuery(function ($) {
         },
     });
 });
+
+jQuery(function ($) {
+    $('.js-faq-question').on('click', function () {
+        $(this).next().slideToggle();
+        $(this).toggleClass('is-open');
+    });
+});
